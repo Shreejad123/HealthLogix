f6f458eedd75031fa1e5c727bbac3eb9401dda3a
https://github.com/Shreejad123 <shreejadkotian00@gmail.com>
2026-08-17 11:42:47 +0530

    updated file name in the piechart pae

--- src/components/pieChart.jsx
+++ src/components/pieChart.jsx
@@ -1,7 +1,7 @@
 import { Chart } from "react-google-charts";
 import styles from "./pieChart.module.css";
 import { useState } from "react";
-import PieChartSkeleton from "./PieChartSkeleton";
+import PieChartSkeleton from "./PiechartSkeleton";
 function PieChart() {
   const [loaded, setLoaded] = useState(false);
   const surgeries = JSON.parse(localStorage.getItem("surgeryList")) || [];
